b68d0461322f7ca651675f97a2bdcd923cbdeee0
BrendonAraujo <brendong.araujo@gmail.com>
2024-01-13 19:09:48 -0300

    #ADD - Entities Mappings

--- FirstRestApi/Business/Models/Address.cs
+++ FirstRestApi/Business/Models/Address.cs
@@ -10,6 +10,7 @@ public class Address : Entity
     public string Complement {  get; set; } = string.Empty;
     public string ZipCode {  get; set; } = string.Empty;
     public string Neighborhood {  get; set; } = string.Empty;
+    public string City {  get; set; } = string.Empty;
     public string State {  get; set; } = string.Empty;
     
     public Supplier? Supplier { get; set; }
